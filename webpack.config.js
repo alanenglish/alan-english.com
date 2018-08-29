52d329dba1bc4ef4282e4ed4664a458b095d6cb0
Alan English <aenglish@manorhillcapital.com>
2018-08-28 22:24:54 -0500

    create project file and folder structure

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// WEBPACK takes all of our assets and bundles them up into a single file (using modules - imports/exports)
+// WEBPACK DEV SERVER - combines server and --watch into one process
+
+const path = require('path')
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env'],
+                    plugins: ['transform-object-rest-spread']
+                }
+            }
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/scripts/'
+    },
+    devtool: 'source-map'
+}
